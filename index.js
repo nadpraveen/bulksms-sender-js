a77c3eb080432b223d6eb7d13f9e4237de4a9031
Praveen Kumar <praveen.42000@gmail.com>
2022-11-20 13:32:01 +0530

    packege created

---
+++ index.js
@@ -0,0 +1,12 @@
+const SMS_URL = 'http://bulksms.techunico.com/vb/apikey.php';
+import axios from 'axios';
+
+function sendSingleSms(smsPayload){
+    axios.post(url, {smsPayload}).then(response => {
+        return response;
+    }).catch(error =>{
+        return error
+    });
+}
+
+module.export.sendSingleSms = sendSingleSms;
