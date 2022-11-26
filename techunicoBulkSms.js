6fbc2bafa981c01d828776bcc1c6b7cf12bca9f4
Praveen Kumar <praveen.42000@gmail.com>
2022-11-26 06:51:58 +0530

    major changes in function

---
+++ techunicoBulkSms.js
@@ -0,0 +1,19 @@
+const SMS_URL = 'http://bulksms.techunico.com/vb/apikey.php';
+import axios from 'axios';
+
+async function sendSingleSms(smsPayload){
+    axios.post(SMS_URL, null, {params: {
+        apikey : smsPayload.apikey,
+        senderid : smsPayload.senderid,
+        templateid : smsPayload.templateid,
+        number : smsPayload.number,
+        message : smsPayload.message,
+    }}).then(response => {
+        console.log(response.data);
+        return response.data;
+    }).catch(error =>{
+        return error
+    });
+}
+
+export default sendSingleSms = sendSingleSms;
